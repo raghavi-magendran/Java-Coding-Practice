bb266fa084162bdef997d860390dddac88814226
raghavi-magendran <167306463+raghavi-magendran@users.noreply.github.com>
2024-04-17 16:35:37 +0100

    Concatination of the array.

---
+++ ArrayConcat.java
@@ -0,0 +1,17 @@
+public class ArrayConcat {
+    public static void main(String[] args) {
+        int[] nums={1,2,1};
+        int n = nums.length;
+        int[] ans = new int[2*n];
+        for(int i=0; i<nums.length; i++)
+       {
+            ans[i] = nums[i];
+            ans[i+n] = nums[i];
+       }
+       
+       for(int j=0;j<ans.length;j++)
+       {System.out.println(ans[j]);}
+    } 
+ }
+
+
