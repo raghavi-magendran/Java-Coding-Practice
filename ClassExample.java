5e345d2f5ce0d282ca8b1902b4b006545b749ae8
raghavi-magendran <167306463+raghavi-magendran@users.noreply.github.com>
2024-04-19 16:08:35 +0100

    new file

--- ClassExample.java
+++ ClassExample.java
@@ -1,10 +1,11 @@
 public class ClassExample {
     public static void main(String[] args) {
-        System.out.println(point(4,5));
+        System.out.println("This is sum: "+point(4,5));
     }
     public static int point(int x, int y)
     {
         int sum = x+y; 
         return sum;
     }
+    
 }
