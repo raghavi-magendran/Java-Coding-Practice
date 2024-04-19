1ffdb369295705730dc4595ca138cff0d5c1cb21
raghavi-magendran <167306463+raghavi-magendran@users.noreply.github.com>
2024-04-19 16:08:04 +0100

    new file

---
+++ ClassExample.java
@@ -0,0 +1,10 @@
+public class ClassExample {
+    public static void main(String[] args) {
+        System.out.println(point(4,5));
+    }
+    public static int point(int x, int y)
+    {
+        int sum = x+y; 
+        return sum;
+    }
+}
